9fad70246df3ac4fd6ad6c998468e62972f7b737
Olivia <reach.olivialegge@gmail.com>
2016-11-20 14:18:38 -0800

    clock basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Your code here
+var time = new Date(Date.now());
+var hour = time.getHours();
+var minute = time.getMinutes();
+var seconds = time.getSeconds();
+
+var changeTime = function(){
+  nIntervId = setInterval(addSecond, 1000);
+};
+
+var addSecond = function(){
+  if (seconds < 59){
+    seconds += 1;
+    $('#clock').html("<p>" + hour + ":" + minute + ":" + seconds + "</p>");
+  }else{
+    seconds = 0;
+    addMinute();
+  }
+};
+
+var addMinute = function(){
+  if (minute < 59){
+    minute += 1;
+    $('#clock').html("<p>" + hour + ":" + minute + ":" + seconds + "</p>");
+  }else{
+    minutes = 0;
+    addHour();
+  }
+};
+
+var addHour = function(){
+  if(hour < 23){
+    hour+=1;
+    $('#clock').html("<p>" + hour + ":" + minute + ":" + seconds + "</p>");
+  }else{
+    hour = 0;
+  }
+};
+
+$( document ).ready(function(){
+  $('#clock').html("<p>" + hour + ":" + minute + ":" +seconds + "</p>");
+  changeTime();
+});
